d634a71b39435ec9d33205f15fa2d88e08b660e2
amir <amir.moemen@gmail.com>
2016-08-23 11:28:17 +0200

    Single marker, removes previous

--- app/src/main/java/com/moemen/android/mapreminder/MapFragment.java
+++ app/src/main/java/com/moemen/android/mapreminder/MapFragment.java
@@ -1,10 +1,13 @@
 package com.moemen.android.mapreminder;
 
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -13,6 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -23,6 +27,7 @@ public class MapFragment extends Fragment {
     private static final String TAG = "FELSÖKNING";
 
     MapView mMapView;
+    Marker marker;
     private GoogleMap googleMap;
 
     private TabSelector mTabSelector;
@@ -68,7 +73,32 @@ public class MapFragment extends Fragment {
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+
+                // Set a listener for marker click.
+
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+                    @Override
+                    public void onMapClick(LatLng point) {
+                        if (marker != null){
+                            marker.remove();
+                        }
+
+                        MarkerOptions options = new MarkerOptions()
+                                .position(new LatLng(point.latitude, point.longitude))
+                                .title("New Marker");
+                        marker = googleMap.addMarker(options);
+                        System.out.println(point.latitude + "---" + point.longitude);
+                    }
+                });
+
+
             }
+
+
+
         });
 
         return v;
